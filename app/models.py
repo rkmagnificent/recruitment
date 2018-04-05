98d21fd2b254d82155a8e99ff320865cf3ce46f1
rmarkham <rmarkham@medbpro.net>
2018-04-05 11:36:56 +0530

    add applicants detail model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class ApplicantsDetails(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    dob = models.DateField()
+    gender = models.CharField(max_length=10)
+    address_line1 = models.CharField(max_length=150)
+    address_line2 = models.CharField(max_length=150)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    phone = models.IntegerField()
+    email = models.EmailField(max_length=255)
+    applied_position = models.CharField(max_length=50)
+    terms_and_conditions = models.BooleanField(default=False)
